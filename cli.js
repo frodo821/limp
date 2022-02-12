473baed50e85dfaa86781737b8e7127046ceabe4
chihiro sakai <sakaic2003@gmail.com>
2022-02-12 22:14:08 +0900

    add shebang to cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const DEFAULT_HTML_TEMPLATE = `<!DOCTYPE html>\
 <html lang="en"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" />\
 <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>$FILENAME</title></head>\
