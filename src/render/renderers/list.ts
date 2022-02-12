6aa09a4a3ba43350c2610ba52f30d240f7f42347
chihiro sakai <sakaic2003@gmail.com>
2022-02-12 16:12:11 +0900

    add list renderer

---
+++ src/render/renderers/list.ts
@@ -0,0 +1,38 @@
+import { Renderer } from '..';
+import { LimpNodeOf } from '../../parsing/parsing';
+import renderer from '../renderer';
+
+class ListRenderer implements Renderer {
+  render(node: LimpNodeOf<'role' | 'block_role'>): string {
+    if (node.type === 'role') {
+      return `<span>${node.children
+        .map((it) => renderer.render(it))
+        .join('')}</span>`;
+    }
+
+    if (node.name === 'list') {
+      const type = node.args[0] || node.args['type'] || 'ordered';
+
+      if (type === 'unordered') {
+        return `<ul>${node.children
+          .map((it) => renderer.render(it))
+          .join('')}</ul>`;
+      }
+
+      return `<ol>${node.children
+        .map((it) => renderer.render(it))
+        .join('')}</ol>`;
+    }
+
+    return `<li>${node.children
+      .map((it) => renderer.render(it))
+      .join('')}</li>`;
+  }
+}
+
+const instance = new ListRenderer();
+
+renderer.registerRenderer('list', instance);
+renderer.registerRenderer('item', instance);
+
+export default {};
