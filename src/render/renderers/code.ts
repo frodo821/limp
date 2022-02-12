b828230d23fb2a5e47c9abb72d42ad5e84ecdfd9
chihiro sakai <sakaic2003@gmail.com>
2022-02-12 23:02:00 +0900

    escape html special characters

--- src/render/renderers/code.ts
+++ src/render/renderers/code.ts
@@ -2,16 +2,29 @@ import { Renderer } from '..';
 import { LimpNodeOf } from '../../parsing/parsing';
 import renderer from '../renderer';
 
+function escape(str: string): string {
+  return str.replace(/[&><"]/g, function (s: string) {
+    return (
+      {
+        '&': '&amp;',
+        '>': '&gt;',
+        '<': '&lt;',
+        '"': '&quot;',
+      }[s] || s
+    );
+  });
+}
+
 class CodeRoleRenderer implements Renderer {
   render(node: LimpNodeOf<'role' | 'block_role'>): string {
     if (node.type === 'role') {
-      return `<code>${node.children
-        .map((it) => renderer.render(it))
-        .join('')}</code>`;
+      return `<code>${escape(
+        node.children.map((it) => renderer.render(it)).join('')
+      )}</code>`;
     }
-    return `<code><pre>${node.children
-      .map((it) => renderer.render(it))
-      .join('')}</pre></code>`;
+    return `<code><pre>${escape(
+      node.children.map((it) => renderer.render(it)).join('')
+    )}</pre></code>`;
   }
 }
 
